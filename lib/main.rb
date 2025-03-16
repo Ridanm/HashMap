d7a688a2830a0aaf860c24d14e1f2af3917b4d38
Ridanm <danyfox1.dm@gmail.com>
2025-03-16 10:27:07 -0300

    rubocop por main

--- lib/main.rb
+++ lib/main.rb
@@ -5,47 +5,38 @@ require_relative 'hash_map'
 
 hash_map = HashMap.new
 
-#p hash_map.hash('Daniel')
-#p hash_map.hash('Rosa')
-#p hash_map.hash('Danie')
-#p hash_map
+# p hash_map.hash('Daniel')
+# p hash_map.hash('Rosa')
+# p hash_map.hash('Danie')
+# p hash_map
 
-hash_map.set('Daniel', { number: '2121e434' })
-hash_map.set('Rosa', mail: 'joder@yop.com')
-hash_map.set('Daniel', { mail: '@dano.com' })
-hash_map.set('Ro', { mail: 'Ro@24co.com' })
+hash_map.set('appel', 'red')
+hash_map.set('banana', 'yellow')
+hash_map.set('carrot', 'orange')
+hash_map.set('dog', 'brown')
+# hash_map.set('Ro', 'data' => { mail: 'Ro@24co.com' })
+# hash_map.set('Daniel', data: { birthday: 'August/2' })
+hash_map.set('banana', 'pink')
 
-
-container = hash_map.buckets
-#p container.class
-#p container[0]
-#p container
-puts 
-#p hash_map.get('Ros')
-#p hash_map.get('Rosa')
-p hash_map.get('niel')
-p hash_map.get('Daniel')
-p hash_map.length
-p hash_map.buckets.length
-p hash_map.keys
-p hash_map.values
-#p hash_map.clear
-#p hash_map.buckets
-p hash_map.remove('Daniel')
-
-#hash_map.buckets[15].head.each { |key, value|  puts key, value }
-
-#p hash_map.buckets[15].update!(15, ['Daniel', { prueba: 'uno' }])
-
-=begin
-puts "\nLinked class"
-array = [] 
-
-l_list = DataStructure::LinkedList.new
-l_list.append(['josh', {nop: 'sip'}])
-l_list.append(['Dan', {age: 32}])
-l_list.append(['Yi', {age: 34}])
-
-array[2] = l_list if array[2].nil?
-p array
-=end
+hash_map.buckets
+# p container.class
+# p container[0]
+# p container
+puts
+# p hash_map.get('Ros')
+# p hash_map.get('Rosa')
+# p hash_map.get('niel')
+# p hash_map.get('Daniel')
+# p hash_map.length
+# p hash_map.buckets.length
+# p hash_map.keys
+# p hash_map.values
+# p hash_map.clear
+# p hash_map.buckets
+# p hash_map.remove('Rosa')
+# p hash_map.remove('Ros')
+# p hash_map.remove('Daniel')
+p hash_map.entries
+# p hash_map.has?('Rosa')
+# p hash_map.has?('Kira')
+# p hash_map.has?('Daniel')
