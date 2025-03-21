44a9da0748e0e5792850151928d37b4807d5e082
Ridanm <danyfox1.dm@gmail.com>
2025-03-21 08:27:42 -0300

    The update method was modified to accept multiple objects

--- lib/complementary_methods.rb
+++ lib/complementary_methods.rb
@@ -1,17 +1,32 @@
 # frozen_string_literal: true
 
+require 'pry-byebug'
 # This module has methods that interact with the HashMap class
 module UpdateData
   def index_error(bucket_index)
     raise IndexError if bucket_index.negative? || bucket_index >= @capacity
   end
 
-  def data(object); end
+  def check_object(object)
+    return unless object.is_a?(Hash)
+
+    p 'Hellloooo' if current.value.last.keys.include?(data.keys)
+  end
 
   def update_key!(key, data)
     current = @buckets[hash(key)].head
     while current
-      current.value[1] = data if current.value[0] == key
+      if current.value.first == key
+        if data.is_a?(Hash)
+          current.value[1].merge!(data)
+        elsif data.is_a?(Array)
+          arr = current.value[1] << data
+          arr.flatten!.compact.uniq!
+        else
+          current.value[1] = data
+        end
+        return
+      end
       current = current.next_node
     end
   end
