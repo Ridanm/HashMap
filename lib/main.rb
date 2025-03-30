7acdba2fb738a3c1ba09747ffd85731747fcc6eb
Ridanm <danyfox1.dm@gmail.com>
2025-03-29 22:57:10 -0300

    odin test added

--- lib/main.rb
+++ lib/main.rb
@@ -4,41 +4,30 @@ require_relative 'chaining'
 require_relative 'complementary_methods'
 require_relative 'hash_map'
 
-hash_map = HashMap.new
+test = HashMap.new
+puts "buckets size => #{test.buckets.size}"
+puts "grouwth => #{test.growth}"
 
-# p hash_map.hash('Daniel')
-# p hash_map.hash('Rosa')
-# p hash_map.hash('Danie')
-# p hash_map
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
 
-hash_map.set('appel', 'red')
-hash_map.set('banana', 'yellow')
-hash_map.set('carrot', 'orange')
-hash_map.set('dog', 'brown')
-# hash_map.set('Ro', 'data' => { mail: 'Ro@24co.com' })
-# hash_map.set('Daniel', data: { birthday: 'August/2' })
-hash_map.set('banana', 'pink')
+puts "grouwth => #{test.growth}"
+puts "buckets size => #{test.buckets.size}"
+puts "entries => #{test.entries.sort}"
 
-hash_map.buckets
-# p container.class
-# p container[0]
-# p container
-puts
-# p hash_map.get('Ros')
-# p hash_map.get('Rosa')
-# p hash_map.get('niel')
-# p hash_map.get('Daniel')
-# p hash_map.length
-# p hash_map.buckets.length
-# p hash_map.keys
-# p hash_map.values
-# p hash_map.clear
-# p hash_map.buckets
-# p hash_map.remove('Rosa')
-# p hash_map.remove('Ros')
-# p hash_map.remove('Daniel')
-p hash_map.entries
-# p hash_map.has?('Rosa')
-# p hash_map.has?('Kira')
-# p hash_map.has?('Daniel')
-
+test.set('moon', 'silver')
+test.set('dog', 'grey')
+test.set('banana', 'green')
+test.set('carrot', 'green')
+puts "\nentries =>  #{test.entries.sort}"
+puts "buckets size => #{test.buckets.size}"
