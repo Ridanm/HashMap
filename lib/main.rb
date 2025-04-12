3322260ee7166f900a9e44b20e43515958752fbd
Ridanm <danyfox1.dm@gmail.com>
2025-04-12 09:14:28 -0300

    HashSet added to main file

--- lib/main.rb
+++ lib/main.rb
@@ -3,10 +3,12 @@
 require_relative 'chaining'
 require_relative 'complementary_methods'
 require_relative 'hash_map'
+require_relative 'hash_set'
 
 test = HashMap.new
-puts "buckets size => #{test.buckets.size}"
-puts "grouwth => #{test.growth}"
+
+# Initial size
+puts "\nbuckets initial size => #{test.buckets.size}"
 
 test.set('apple', 'red')
 test.set('banana', 'yellow')
@@ -21,13 +23,51 @@ test.set('jacket', 'blue')
 test.set('kite', 'pink')
 test.set('lion', 'golden')
 
-puts "grouwth => #{test.growth}"
-puts "buckets size => #{test.buckets.size}"
-puts "entries => #{test.entries.sort}"
+# Entries
+puts "\nentries => #{test.entries.sort}"
 
-test.set('moon', 'silver')
-test.set('dog', 'grey')
+# Update values
 test.set('banana', 'green')
 test.set('carrot', 'green')
 puts "\nentries =>  #{test.entries.sort}"
-puts "buckets size => #{test.buckets.size}"
+
+# Re_hash
+test.set('moon', 'silver')
+puts "\nbuckets re_hash  size => #{test.buckets.size}"
+
+# Only keys
+puts "\nOnly keys => #{test.keys.sort}"
+
+# Get key
+puts "\nget(banana) => #{test.get('banana')}"
+
+#other = HashMap.new(8)
+#other.set('aa', 'blue')
+#other.set('bb', 'red')
+#other.set('aba', 'yellow')
+#p other.buckets[0]
+#other.remove('aa')
+#puts
+#p other.buckets[0]
+
+# HashSet
+hash_set = HashSet.new(8)
+#p hash_set.instance_of?(HashSet)
+#p hash_set.class
+#p hash_set.set('Rosa')
+#p hash_set.buckets[3].class
+#p hash_set.buckets.size
+
+hash_set.set('aa')
+hash_set.set('bb')
+hash_set.set('aba')
+#p hash_set.remove('aa')
+#p hash_set.remove('bb')
+
+# HashSet entries
+puts "\nHashSet#entries =>  #{ hash_set.entries}"
+# HashSet capacity
+puts "\nHashSet#capacity => #{hash_set.capacity}"
+
+# re_hash
+puts "\nHashSet#re_hash => #{hash_set.re_hash.buckets.size}"
